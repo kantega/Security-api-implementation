62d02f7452e5e4d80fdc5bb7f32974b431dec78c
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-05-07 16:20:51 +0000

    Removed invalid char
    git-svn-id: https://opensource.kantega.no/svn/security-api-impl/trunk@5388 fd808399-8219-4f14-9d4c-37719d9ec93d

--- dbuser/src/main/java/no/kantega/security/api/impl/dbuser/password/PasswordCrypt.java
+++ dbuser/src/main/java/no/kantega/security/api/impl/dbuser/password/PasswordCrypt.java
@@ -18,11 +18,6 @@ package no.kantega.security.api.impl.dbuser.password;
 
 import java.security.NoSuchAlgorithmException;
 
-/**
- * Author: Kristian Lier Selnæs, Kantega AS
- * Date: 27.mai.2008
- * Time: 13:30:08
- */
 public interface PasswordCrypt {
 
     public String crypt(String password) throws NoSuchAlgorithmException;
