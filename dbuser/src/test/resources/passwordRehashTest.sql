92b970d2d455eb9f3e590099dae5c9eec58e3087
Sigurd Stendal <Sigurd.Stendal@kantega.no>
2014-05-08 09:07:53 +0000

    AP-1589
    git-svn-id: https://opensource.kantega.no/svn/security-api-impl/trunk@5403 fd808399-8219-4f14-9d4c-37719d9ec93d

---
+++ dbuser/src/test/resources/passwordRehashTest.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dbuserpassword
+(
+    Domain VARCHAR(64),
+    UserId VARCHAR(64),
+    Password VARCHAR(255)
+);
+
+INSERT INTO dbuserpassword (Domain, UserId, Password) VALUES('my domain', 'jason', '5f4dcc3b5aa765d61d8327deb882cf99');
